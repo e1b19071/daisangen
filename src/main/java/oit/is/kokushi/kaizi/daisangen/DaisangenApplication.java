cca8f2a4646110e9776696fca5e0ba4683bee6ab
shoma2491 <e1b19062@st.oit.ac.jp>
2021-10-05 14:48:56 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/kokushi/kaizi/daisangen/DaisangenApplication.java
@@ -0,0 +1,13 @@
+package oit.is.kokushi.kaizi.daisangen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisangenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaisangenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/kokushi/kaizi/daisangen/DaisangenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.kokushi.kaizi.daisangen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaisangenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
