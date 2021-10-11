1dc0f0ee24aa6eee451ba335b1acbf3eba875e05
NaRHa0713 <e1b19104@oit.ac.jp>
2021-10-11 18:53:23 +0900

    入力された整数値を全部表示して足す

--- src/main/java/oit/is/kokushi/kaizi/daisangen/controller/Sample26Controller.java
+++ src/main/java/oit/is/kokushi/kaizi/daisangen/controller/Sample26Controller.java
@@ -26,4 +26,25 @@ public class Sample26Controller {
   public String sample26() {
     return "sample26.html";
   }
+
+  /**
+   * メソッド名は異なるが，/sample26というPOSTリクエストがあったら，こちらが呼び出されて，sample26.htmlが返る．一つのリクエストに対してPOST，GET両方を使い分けることができる
+   * ArrayListオブジェクトをModelMapの変数に直接与えて，thymeleafにわたすことができる
+   *
+   * @return
+   */
+  @PostMapping
+  public String sample27(@RequestParam Integer min, @RequestParam Integer max, ModelMap model) {
+    int sum = 0;
+    ArrayList<Integer> sumRange = new ArrayList<>();
+    // minからmaxまでの数値を加算し，sumに代入する
+    // 同時にminからmaxまでの数値を順にArrayListに格納する
+    for (int i = min; i <= max; i++) {
+      sum = sum + i;
+      sumRange.add(i);
+    }
+    model.addAttribute("sumRange", sumRange);
+    model.addAttribute("sum", sum);
+    return "sample26.html";
+  }
 }
